e1282a21f8ec6f3944c847ba76b323999c0fe504
Kaan <kaanmontplaisir@yahoo.com>
2022-06-21 16:47:51 -0500

    Initial Project

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const product = require('./api/product')
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use("/api/product", product)
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`,));
